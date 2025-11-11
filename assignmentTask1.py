5d2123da1bca523839f460638ae9da34c472510a
chulbulikushi07 <chulbulikushi07@gmail.com>
2025-11-11 16:14:29 +0530

    initial commit

---
+++ assignmentTask1.py
@@ -0,0 +1,12 @@
+num1 =int(input("Enter the first number "))
+num2 =int(input("Enter the second number "))
+
+sum = num1+num2
+sub = num1-num2
+product = num1*num2
+division = num1/num2
+
+print("Addition:",sum)
+print("subtraction:",sub)
+print("multiplication:",product)
+print("division:",division)
